f65e97252f72120d50cfd0085331d6f312cc8383
Dicle Kartal <20MISY1013@isik.edu.tr>
2026-03-11 19:55:50 +0300

    Refactor InventoryService to manage medicines

--- src/main/java/org/mavensample/service/InventoryService.java
+++ src/main/java/org/mavensample/service/InventoryService.java
@@ -1,5 +1,33 @@
-package org.mavensample.service;
+package org.pharmacy.service;
+
+import org.pharmacy.dao.MedicineDao;
+import org.pharmacy.model.Medicine;
 
 public class InventoryService {
-    
+
+    private MedicineDao medicineDao;
+
+    public InventoryService() {
+        medicineDao = new MedicineDao();
+    }
+
+    // Yeni ilaç eklemesi
+    public void addMedicine(Medicine medicine) {
+        medicineDao.addMedicine(medicine);
+    }
+
+    // Tüm ilaçları listeliyicejk
+    public void listMedicines() {
+        medicineDao.getAllMedicines();
+    }
+
+    // Stok güncelleme
+    public void updateStock(int id, int newStock) {
+        medicineDao.updateMedicineStock(id, newStock);
+    }
+
+    // İlaç silme
+    public void deleteMedicine(int id) {
+        medicineDao.deleteMedicine(id);
+    }
 }
