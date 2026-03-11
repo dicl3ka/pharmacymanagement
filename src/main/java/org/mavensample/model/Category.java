e02d64f1f9140e18f4d7c3a57de23d9552068d14
Dicle Kartal <20MISY1013@isik.edu.tr>
2026-03-11 19:54:40 +0300

    Refactor Category class with fields and methods
    Updated package name and added fields with constructors, getters, and setters.

--- src/main/java/org/mavensample/model/Category.java
+++ src/main/java/org/mavensample/model/Category.java
@@ -1,5 +1,36 @@
-package org.mavensample.model;
+package org.pharmacy.model;
 
 public class Category {
-    
+
+    private int id;
+    private String categoryName;
+
+    // Constructor (boş)
+    public Category() {
+    }
+
+    public Category(int id, String categoryName) {
+        this.id = id;
+        this.categoryName = categoryName;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    // Setter
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
